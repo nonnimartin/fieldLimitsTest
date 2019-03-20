47733ecea59b901e1c0c3f519b6dfa7406c7aa2e
Jonathan Martin <jonathan.martin@lucidworks.com>
2019-03-20 13:44:02 -0700

    slight adjustment to finding highest hex and to readme

--- README.md
+++ README.md
@@ -6,4 +6,6 @@ Requires enlighten library. This can be installed with `pip install enlighten`
 
 Use config.json to configure Solr endpoint, and to set the number of fields, documents, and the size of json payload for each request.
 
--c flag will override the config.json "commit" setting and set the requests to commit=true, otherwise the script defaults to the configuration.
+-c flag will override the config.json "commit" setting and set the requests to commit=true, otherwise the script defaults to the configuration.i
+
+-i flag will enable "incremental" creation of new fields, starting from where the last run of the script left off
--- fieldLimitsTest.py
+++ fieldLimitsTest.py
@@ -27,7 +27,7 @@ def get_latest_docnum(endpoint):
 
     print 'Getting last Doc Number'
     for i in range(0, 100):
-        print 'Making request. Trying ' + str(i + 1) + ' times.'
+        print 'Making request for last doc. Trying ' + str(i + 1) + ' times.'
         try:
             r = requests.get(endpoint)
             r.raise_for_status()
@@ -49,7 +49,7 @@ def get_latest_fieldnum(endpoint):
     print 'Getting last Hex Number'
 
     for i in range(0, 100):
-        print 'Making request. Trying ' + str(i + 1) + ' times.'
+        print 'Making request for latest field. Trying ' + str(i + 1) + ' times.'
         try:
             r = requests.get(endpoint)
             r.raise_for_status()
@@ -68,7 +68,7 @@ def get_latest_fieldnum(endpoint):
     doc = docsList[0]
     for field in doc.keys():
         if field.startswith('x-manyFieldsTest-'):
-            thisNumber = field.split('x-manyFieldsTest-')[1]
+            thisNumber = field.split('x-manyFieldsTest-')[1].split('_')[0]
             decNum     = int(thisNumber, 16)
             if decNum > highestNum:
                 highestNum = decNum
